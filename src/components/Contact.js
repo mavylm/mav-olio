3d89372512b003474936d9e84ffed92ac7620cd1
Mavis Lim <81512500+mavylm@users.noreply.github.com>
2022-02-09 23:19:32 +0800

    Updated portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   FaDiscord,
   FaFacebookMessenger,
@@ -7,77 +7,77 @@ import {
   FaTwitch,
   FaLinkedinIn,
   FaEnvelope,
-} from "react-icons/fa";
-import Fade from "react-reveal/Fade";
+} from 'react-icons/fa';
+import Fade from 'react-reveal/Fade';
 
 const Contact = () => {
   return (
-    <div className="content-wrapper">
-      <div className="content">
-        <div className="contact">
+    <div className='content-wrapper'>
+      <div className='content'>
+        <div className='contact'>
           <Fade top>
             <h1>Let's Create Something Together!</h1>
             <p>Or say Hello!</p>
           </Fade>
           <Fade left>
-            <span className="divider"></span>
+            <span className='divider'></span>
           </Fade>
           <Fade bottom>
             <p>
               <ul>
                 <li>
                   <a
-                    href="https://discord.com/app"
-                    title="mavy#9999"
-                    target="_blank"
-                    rel="noreferrer"
+                    href='https://discord.com/app'
+                    title='mavy#3799'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <FaDiscord />
                   </a>
                 </li>
                 <li>
-                  <a href="http://m.me/mavylm" target="_blank" rel="noreferrer">
+                  <a href='http://m.me/mavylm' target='_blank' rel='noreferrer'>
                     <FaFacebookMessenger />
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://github.com/mavylm"
-                    target="_blank"
-                    rel="noreferrer"
+                    href='https://github.com/mavylm'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <FaGithub />
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://www.instagram.com/mavylm/"
-                    target="_blank"
-                    rel="noreferrer"
+                    href='https://www.instagram.com/mavylm/'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <FaInstagram />
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://www.twitch.tv/azukera"
-                    target="_blank"
-                    rel="noreferrer"
+                    href='https://www.twitch.tv/azukera'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <FaTwitch />
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://www.linkedin.com/in/mavislimhm/"
-                    target="_blank"
-                    rel="noreferrer"
+                    href='https://www.linkedin.com/in/mavislimhm/'
+                    target='_blank'
+                    rel='noreferrer'
                   >
                     <FaLinkedinIn />
                   </a>
                 </li>
                 <li>
-                  <a href="mailto:mavislimhm@gmail.com">
+                  <a href='mailto:mavislimhm@gmail.com'>
                     <FaEnvelope />
                   </a>
                 </li>
